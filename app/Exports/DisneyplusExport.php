b560c2a98eeb1f720916ad6e7fbf80f0031c3224
aungphyohtwe <tharhtwe2013@gmail.com>
2022-07-08 13:05:51 +0900

    second commit

--- app/Exports/DisneyplusExport.php
+++ app/Exports/DisneyplusExport.php
@@ -23,4 +23,39 @@ class DisneyplusExport implements FromView
             'disneyplus' => Disneyplus::where('id', $this->proj_id)->get()
         ]);
     }
+
+    function wareki($year) {
+
+        $eras = [
+            ['year' => 2018, 'name' => '令和'],
+            ['year' => 1988, 'name' => '平成'],
+            ['year' => 1925, 'name' => '昭和'],
+            ['year' => 1911, 'name' => '大正'],
+            ['year' => 1867, 'name' => '明治']
+        ];
+    
+        foreach($eras as $era) {
+    
+            $base_year = $era['year'];
+            $era_name = $era['name'];
+    
+            if($year > $base_year) {
+    
+                $era_year = $year - $base_year;
+    
+                if($era_year === 1) {
+    
+                    return $era_name .'元年';
+    
+                }
+    
+                return $era_name . $era_year .'年';
+    
+            }
+    
+        }
+    
+        return null;
+    
+    }
 }
